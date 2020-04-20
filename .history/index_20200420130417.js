ff525f1b2b262e4b49b9335dc51c56008ada3da6
iquery-001 <kota@vaga.tk>
2020-04-20 13:06:24 -0500

    Done.

--- .history/index_20200420130417.js
+++ .history/index_20200420130417.js
@@ -6,5 +6,7 @@ const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push('Odie');
 prepend.unshift('Odie');
-removeLast.splice(-1, 1);
-removeFirst.splice(0, 1);
+removeLast.pop;
+removeLast;
+removeFirst.shift;
+removeFirst;
